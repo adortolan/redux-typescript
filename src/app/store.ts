167f2ced65f95ed40c24ffa1593dbf835167708d
adortolan <adortolan@gmail.com>
2024-03-21 19:46:19 -0300

    feat: Add userReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import postsReducer from "../features/posts/postsSlice";
+import userReducer from "../features/user/userSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     posts: postsReducer,
+    users: userReducer,
   },
 });
 
