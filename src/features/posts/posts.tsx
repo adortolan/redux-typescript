eb7bb1a52c46a5ae624ca73b4f71b885e3e599ed
adortolan <adortolan@gmail.com>
2024-03-21 19:47:38 -0300

    feat: Add orderedPosts

--- src/features/posts/posts.tsx
+++ src/features/posts/posts.tsx
@@ -4,11 +4,14 @@ import { selectPosts } from "./postsSlice";
 
 export const Posts = () => {
   const posts = useAppSelector(selectPosts);
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {posts.map((post) => (
+      {orderedPosts.map((post) => (
         <article className="post-excerpt" key={post.id}>
           <Link to={`/post/${post.id}`}>
             <h3>{post.title}</h3>
