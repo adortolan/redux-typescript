180ce2a9c3d01e230858fe56a5da98e1269a9e4e
adortolan <adortolan@gmail.com>
2024-03-21 19:48:48 -0300

    feat: Add news fields

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,24 +1,52 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../../app/store";
 
 type Post = {
   id: string;
+  date: string;
   title: string;
   content: string;
+  user: string;
 };
 
 const initialState: Post[] = [
-  { id: "1", title: "First Post", content: "Hello!" },
-  { id: "2", title: "Second Post", content: "content posted!" },
+  {
+    id: "1",
+    date: new Date().toISOString(),
+    title: "First Post",
+    content: "Hello!",
+    user: "01",
+  },
+  {
+    id: "2",
+    date: new Date().toISOString(),
+    title: "Second Post",
+    content: "content posted!",
+    user: "02",
+  },
 ];
 
 export const postSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    postAdd(state, action) {
-      state.push(action.payload);
+    postAdd: {
+      reducer(state, action: PayloadAction<Post>) {
+        state.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            date: new Date().toISOString(),
+            title,
+            content,
+            user: userId,
+          },
+        };
+      },
     },
+
     postUpdate(state, action) {
       const { id, title, content } = action.payload;
       const existPost = state.find((post) => post.id === id);
