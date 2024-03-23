322e8d6fdf795acf8ff63579e1554b5f9cb5484c
adortolan <adortolan@gmail.com>
2024-03-23 13:54:42 -0300

    feat: Add ReactionButtons form

--- src/features/posts/posts.tsx
+++ src/features/posts/posts.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../app/hooks";
 import { selectPosts } from "./postsSlice";
+import { ReactionButtons } from "./ReactionButtons";
 
 export const Posts = () => {
   const posts = useAppSelector(selectPosts);
@@ -17,6 +18,7 @@ export const Posts = () => {
             <h3>{post.title}</h3>
           </Link>
           <p className="post-content">{post.content.substring(0, 100)}</p>
+          <ReactionButtons post={post} />
         </article>
       ))}
     </section>
